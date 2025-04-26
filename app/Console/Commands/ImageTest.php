fa2e4c081dbfd47889412ba2c9c5b611f9021d38
Connor Tumbleson <connor@sourcetoad.com>
2025-04-26 09:12:18 -0400

    feat: simple image test

---
+++ app/Console/Commands/ImageTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use OpenAI\Laravel\Facades\OpenAI;
+
+class ImageTest extends Command
+{
+    protected $signature = 'app:image-test';
+    protected $description = 'Command description';
+
+    public function handle()
+    {
+        $response = OpenAI::images()->create([
+            'model' => 'gpt-image-1',
+            'prompt' => 'A cute baby sea otter',
+            'size' => '1024x1024',
+        ]);
+
+        dd($response);
+    }
+}
