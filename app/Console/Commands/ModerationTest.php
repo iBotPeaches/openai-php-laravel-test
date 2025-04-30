87313add93f58f0c3a3a9fd4909e9b52c2a2c847
Connor Tumbleson <connor@sourcetoad.com>
2025-04-30 10:35:33 -0400

    feat: add moderation test sample

---
+++ app/Console/Commands/ModerationTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use OpenAI\Laravel\Facades\OpenAI;
+
+class ModerationTest extends Command
+{
+    protected $signature = 'app:moderation-test';
+
+    protected $description = 'Command description';
+
+    public function handle()
+    {
+        $result = OpenAI::moderations()->create([
+            'model' => 'omni-moderation-latest',
+            'input' => 'I want to kill spiders in my house, I am not mean. I just do not like them.'
+        ]);
+
+        dd($result);
+    }
+}
