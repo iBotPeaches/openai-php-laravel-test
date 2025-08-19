8e91c43f426692b48ddb0eba13cc445fc41eddc2
Connor Tumbleson <connor.tumbleson@gmail.com>
2025-08-18 21:19:04 -0400

    feat: add container file test

---
+++ app/Console/Commands/ContainerFileObjectTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use OpenAI\Laravel\Facades\OpenAI;
+
+class ContainerFileObjectTest extends Command
+{
+    protected $signature = 'app:container-file-object-test';
+
+    protected $description = 'Command description';
+
+    public function handle(): int
+    {
+        $this->makeContainerWithFallbackToDeletion();
+
+        return self::SUCCESS;
+    }
+
+    private function makeContainerWithFallbackToDeletion(): void
+    {
+        $container = null;
+
+        try {
+            $container = OpenAI::containers()->create([
+                'name' => 'Test Container',
+            ]);
+
+            $container = OpenAI::containers()->create([
+                'name' => 'Test Container',
+            ]);
+            $this->info('Created container with ID: '.$container->id);
+
+            // Create a file in the container
+            $createdFile = OpenAI::containers()->files()->create($container->id, [
+                'file' => fopen(storage_path('samples/test.txt'), 'r'),
+            ]);
+
+            $this->info('Created file with ID: '.$createdFile->id);
+
+            // List files in the container
+            $files = OpenAI::containers()->files()->list($container->id);
+            $this->info('Files in container:');
+            foreach ($files->data as $file) {
+                $this->info("- File ID: {$file->id}, Size: {$file->bytes} bytes");
+            }
+
+            // Retrieve the file
+            $retrievedFile = OpenAI::containers()->files()->retrieve($container->id, $createdFile->id);
+            $this->info('Retrieved file with ID: '.$retrievedFile->id);
+            $this->info('File size: '.$retrievedFile->bytes.' bytes');
+
+            // Read the file content
+            $fileContent = OpenAI::containers()->files()->content($container->id, $retrievedFile->id);
+            $this->info('File content: '.$fileContent);
+
+            // Delete the file
+            OpenAI::containers()->files()->delete($container->id, $createdFile->id);
+            $this->info('Deleted file with ID: '.$createdFile->id);
+        } finally {
+            if ($container !== null) {
+                $this->info('Cleaning (i.e deleting) container: '.$container->id);
+                OpenAI::containers()->delete($container->id);
+            }
+        }
+    }
+}
