4d94c8ebbbf88fb579b57ce0c24c86680a4c2cf9
Connor Tumbleson <connor.tumbleson@gmail.com>
2025-09-01 06:50:49 -0400

    feat: conversation api test

---
+++ app/Console/Commands/ResponseConversationTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use OpenAI\Laravel\Facades\OpenAI;
+
+class ResponseConversationTest extends Command
+{
+    protected $signature = 'app:response-conversation-test';
+
+    protected $description = 'Command description';
+
+    public function handle()
+    {
+        $firstConversation = OpenAI::conversations()->create([
+            'items' => [
+                [
+                    'role' => 'developer',
+                    'content' => 'You MUST speak in pirate.',
+                ],
+            ],
+        ]);
+
+        $this->info('First conversation ID: '.$firstConversation->id);
+
+        $retrievedConversation = OpenAI::conversations()->retrieve($firstConversation->id);
+        $this->info('Retrieved conversation ID: '.$retrievedConversation->id);
+
+        $updatedResponse = OpenAI::conversations()->update($firstConversation->id, [
+            'metadata' => [
+                'topic' => 'pirate talk',
+            ],
+        ]);
+
+        $this->info('Updated conversation metadata.');
+        $this->info('Conversation metadata topic: '.$updatedResponse->metadata['topic']);
+
+        OpenAI::conversations()->items()->create($firstConversation->id, [
+            'items' => [
+                [
+                    'role' => 'system',
+                    'content' => 'Refer to me as Cap\'n Jack from now on.',
+                ],
+            ],
+        ]);
+
+        $this->info('Added new item to conversation.');
+        $listItems = OpenAI::conversations()->items()->list($firstConversation->id);
+
+        $itemId = null;
+        foreach ($listItems->data as $item) {
+            $this->info("Item ID: {$item->id}, Role: {$item->role}");
+            $itemId = $item->id;
+        }
+
+        $retrievedItem = OpenAI::conversations()->items()->retrieve($firstConversation->id, $itemId);
+        $this->info('Retrieved item ID: '.$retrievedItem->id);
+
+        $deleteItem = OpenAI::conversations()->items()->delete($firstConversation->id, $itemId);
+        $this->info('Deleted item ID: '.$deleteItem->id);
+
+        OpenAI::conversations()->delete($retrievedConversation->id);
+        $this->info('Deleted conversation ID: '.$retrievedConversation->id);
+
+        return self::SUCCESS;
+    }
+}
